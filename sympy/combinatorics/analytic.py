466507ab32947bd2b53ed7dbff584a4bb1a3f23d
hm <hacman0@gmail.com>
2013-04-21 09:38:59 -0400

    more Analytic Combinatorics constructions

--- sympy/combinatorics/analytic.py
+++ sympy/combinatorics/analytic.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
+'''
+Analytic Combinatorics
+Sedgewick, R. and P. Flajolet. Analytic Combinatorics. Cambridge University
+Press, 2007.
+'''
 
 from sympy.core.assumptions import ManagedProperties
-from sympy.core import Basic
 from sympy.abc import z
+from sympy.core import Basic, Dummy
+from sympy import oo, exp, log, Sum, totient
+
 
 class Multiton(ManagedProperties):
     '''
@@ -83,8 +90,43 @@ class SEQ(CombinatorialClass):
     def __new__(cls, other):
         c = CombinatorialClass.__new__(cls)
         c.a = other
-        c._gf = 1/(1-other.gf)
+        c._gf = 1/(1-c.a.gf)
         return c
 
     def __str__(self):
         return 'SEQ(' + str(self.a) + ')'
+
+class MSET(CombinatorialClass):
+    def __new__(cls, other):
+        c = CombinatorialClass.__new__(cls)
+        c.a = other
+        k = Dummy('k', integer=True)
+        c._gf = exp(Sum(c.a.gf.subs(z, z**k)/k, (k, 1, oo)))
+        return c
+
+    def __str__(self):
+        return 'MSET(' + str(self.a) + ')'
+
+class PSET(CombinatorialClass):
+    def __new__(cls, other):
+        c = CombinatorialClass.__new__(cls)
+        c.a = other
+        k = Dummy('k', integer=True)
+        c._gf = exp(Sum((-1)**(k-1)*c.a.gf.subs(z, z**k)/k, (k, 1, oo)))
+        return c
+
+    def __str__(self):
+        return 'PSET(' + str(self.a) + ')'
+
+class CYC(CombinatorialClass):
+    def __new__(cls, other):
+        c = CombinatorialClass.__new__(cls)
+        c.a = other
+        k = Dummy('k', integer=True)
+        c._gf = Sum(totient(k)/k) * log(1/(1-c.a.gf.subs(z,z**k)), (k, 1, oo))
+        return c
+
+    def __str__(self):
+        return 'CYC(' + str(self.a) + ')'
+
+# restricted constructions (p.30)
